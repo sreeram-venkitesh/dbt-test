575436da9f56323c8d59a0676c0bc1781fc6e8e5
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 15:55:50 +0530

    Config fix

--- macros/test.sql
+++ macros/test.sql
@@ -2,7 +2,7 @@
 
 {% set sql %}
   SELECT CONCAT( 'DROP TABLE ', string_agg(table_name,',') , ' CASCADE;' ) 
-  into statement FROM information_schema.tables 
+  AS statement FROM information_schema.tables 
   WHERE table_name LIKE '_airbyte%'; 
 {% endset %}
 
