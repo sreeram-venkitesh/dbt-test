0ae9d2da1a40876c787afae306c091ac19561c33
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 20:38:16 +0530

    Config fix

--- macros/test.sql
+++ macros/test.sql
@@ -1,12 +1,13 @@
 {% macro drop_tables() %}
 
-{% set fetch_items_query %}
-  SELECT CONCAT( 'DROP TABLE ', string_agg('cms_synthetic_patient_data_omop.' || table_name, ',') , ' CASCADE;' ) AS statement FROM information_schema.tables WHERE table_schema = 'cms_synthetic_patient_data_omop' AND table_name LIKE '_airbyte%';
-{% endset %}
 
-{% set results_list = run_query(fetch_items_query).columns[0].values()[0] %}
 
 {% if execute %}
+  {% set fetch_items_query %}
+  SELECT CONCAT( 'DROP TABLE ', string_agg('cms_synthetic_patient_data_omop.' || table_name, ',') , ' CASCADE;' ) AS statement FROM information_schema.tables WHERE table_schema = 'cms_synthetic_patient_data_omop' AND table_name LIKE '_airbyte%';
+  {% endset %}
+
+  {% set results_list = run_query(fetch_items_query).columns[0].values()[0] %}
   {{ run_query(results_list) }}
 {% endif %}
 
