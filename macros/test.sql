6338c06f8612c83594e4d0615e3b4bab6899fbda
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 20:03:51 +0530

    Config fix

--- macros/test.sql
+++ macros/test.sql
@@ -6,9 +6,9 @@
 
 
 {% if execute %}
-{% set results_list = run_query(fetch_items_query).columns[0].values() %}
+{% set results_list = run_query(fetch_items_query).columns[0].values()[0] %}
 {{ log("results list", info=True)}}
-{{ log(results_list[0], info=True)}}
+{{ log(results_list, info=True)}}
 {{ run_query(results_list[0]) }}
 {% endif %}
 
