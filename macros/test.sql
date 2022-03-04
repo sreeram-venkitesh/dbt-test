2cdcc8799c0964d8474f806930a488e5643120ae
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 14:38:02 +0530

    Config fix

--- macros/test.sql
+++ macros/test.sql
@@ -1,30 +1,18 @@
-{% macro delete_tables() %}
-{% set sql %}
-    CREATE OR REPLACE FUNCTION wild_func(IN _schema TEXT, IN _wildcard TEXT)
-    RETURNS void
-    LANGUAGE plpgsql
-    AS
-    $$
-    DECLARE
-      row record;
-    BEGIN
-      FOR row IN
-          SELECT table_schema, table_name
-          FROM information_schema.tables
-          WHERE table_type = 'BASE TABLE'
-          AND table_schema = _schema
-          AND table_name ILIKE (_wildcard || '%')
-      LOOP
-          EXECUTE 'DROP TABLE ' || quote_ident(row.table_schema) || '.' || quote_ident(row.table_name) || ' CASCADE';
-          RAISE INFO 'Dropped tables: %', quote_ident(row.table_schema) || '.' || quote_ident(row.table_name);
-      END LOOP;
-    END;
-    $$;
+{% macro drop_tables(
+  schema,
+  tables = []
+) %}
 
-    SELECT wild_func('cms_synthetic_patient_data_omop', '_airbyte');
+{% set sql %}
+  drop table 
+  {% for table in tables %}
+    {{ schema }}.{{ table }}
+    {% if not loop.last %},{% endif %}
+  {% endfor %}
+  cascade
 {% endset %}
 
-{% do run_query(sql) %}
+{%- if execute -%}
+  {{ run_query(sql) }}
+{% endif %}
 {% endmacro %}
-
-
--- models/select.sql
+++ models/select.sql
@@ -18,4 +18,4 @@ with tables as (
   and table_type = 'BASE TABLE' 
 )
 
-select * from tables
+select * from drop_tables('cms_synethetic_patient_data_omop',tables)
