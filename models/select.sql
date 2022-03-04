ad63431639f40ea829597af7b0ada26688d364c7
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 14:21:50 +0530

    Config fix

--- models/select.sql
+++ models/select.sql
@@ -1,14 +1,21 @@
-with delete as (
-  for row in
-      select table_schema, table_name
-      from information_schema.tables
-      where table_type = 'BASE TABLE'
-      and table_schema = 'cms_synthetic_patient_data_omop'
-      and table_name ILIKE ('_airbyte' || '%')
-  loop
-      execute 'DROP TABLE ' || quote_ident(row.table_schema) || '.' || quote_ident(row.table_name) || ' CASCADE';
-      raise info 'Dropped tables: %', quote_ident(row.table_schema) || '.' || quote_ident(row.table_name);
-  end loop;
-) 
+-- with delete as (
+--   for row in
+--       select table_schema, table_name
+--       from information_schema.tables
+--       where table_type = 'BASE TABLE'
+--       and table_schema = 'cms_synthetic_patient_data_omop'
+--       and table_name ILIKE ('_airbyte' || '%')
+--   loop
+--       execute 'DROP TABLE ' || quote_ident(row.table_schema) || '.' || quote_ident(row.table_name) || ' CASCADE';
+--       raise info 'Dropped tables: %', quote_ident(row.table_schema) || '.' || quote_ident(row.table_name);
+--   end loop;
+-- ) 
+with tables as (
+  select table_name
+  from information_schema.tables
+  where table_name like '_airbyte%'
+  and table_schema not in ('information_schema', 'pg_catalog')
+  and table_type = 'BASE TABLE' 
+)
 
-select * from delete
+drop table * from tables cascade
