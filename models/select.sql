a8fcb3506c075b1b893c98bbedcf7fdf9045acdd
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 14:09:01 +0530

    Config fix

--- models/select.sql
+++ models/select.sql
@@ -1,1 +1,14 @@
-{{ delete_tables() }}
+delete as (
+   FOR row IN
+      SELECT table_schema, table_name
+      FROM information_schema.tables
+      WHERE table_type = 'BASE TABLE'
+      AND table_schema = 'cms_synthetic_patient_data_omop'
+      AND table_name ILIKE ('_airbyte' || '%')
+  LOOP
+      EXECUTE 'DROP TABLE ' || quote_ident(row.table_schema) || '.' || quote_ident(row.table_name) || ' CASCADE';
+      RAISE INFO 'Dropped tables: %', quote_ident(row.table_schema) || '.' || quote_ident(row.table_name);
+  END LOOP;
+) 
+
+select * from delete
