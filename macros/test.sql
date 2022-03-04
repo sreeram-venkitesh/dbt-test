9d3c0c3c6c9f09b0a515b9716b0107d513abba4b
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 15:53:20 +0530

    Config fix

--- macros/test.sql
+++ macros/test.sql
@@ -1,11 +1,12 @@
 {% macro drop_tables() %}
 
 {% set sql %}
-  SET @del = (SELECT CONCAT( 'DROP TABLE ', string_agg(table_name,',') , ' CASCADE;' ) 
+  SELECT CONCAT( 'DROP TABLE ', string_agg(table_name,',') , ' CASCADE;' ) 
   into statement FROM information_schema.tables 
   WHERE table_name LIKE '_airbyte%'); 
-  statement;
 {% endset %}
+
+{{ log(run_query(sql)) }}
 
 {%- if execute -%}
   {{ run_query(run_query(sql)) }}
