66833d3b1d430e1dff035d71a2de5b13f5e9a777
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 14:13:18 +0530

    Config fix

--- models/select.sql
+++ models/select.sql
@@ -1,14 +1,14 @@
 with delete as (
-   FOR row IN
-      SELECT table_schema, table_name
-      FROM information_schema.tables
-      WHERE table_type = 'BASE TABLE'
-      AND table_schema = 'cms_synthetic_patient_data_omop'
-      AND table_name ILIKE ('_airbyte' || '%')
-  LOOP
-      EXECUTE 'DROP TABLE ' || quote_ident(row.table_schema) || '.' || quote_ident(row.table_name) || ' CASCADE';
-      RAISE INFO 'Dropped tables: %', quote_ident(row.table_schema) || '.' || quote_ident(row.table_name);
-  END LOOP;
+  for row in
+      select table_schema, table_name
+      from information_schema.tables
+      where table_type = 'BASE TABLE'
+      and table_schema = 'cms_synthetic_patient_data_omop'
+      and table_name ILIKE ('_airbyte' || '%')
+  loop
+      execute 'DROP TABLE ' || quote_ident(row.table_schema) || '.' || quote_ident(row.table_name) || ' CASCADE';
+      raise info 'Dropped tables: %', quote_ident(row.table_schema) || '.' || quote_ident(row.table_name);
+  end loop;
 ) 
 
 select * from delete
