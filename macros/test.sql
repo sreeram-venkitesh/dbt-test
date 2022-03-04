71f4d0c18ef6c140f3bab1d468825a7935d93bc8
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 20:40:03 +0530

    Config fix

--- macros/test.sql
+++ macros/test.sql
@@ -7,7 +7,8 @@
   SELECT CONCAT( 'DROP TABLE ', string_agg('cms_synthetic_patient_data_omop.' || table_name, ',') , ' CASCADE;' ) AS statement FROM information_schema.tables WHERE table_schema = 'cms_synthetic_patient_data_omop' AND table_name LIKE '_airbyte%';
   {% endset %}
 
-  {% set results_list = run_query(fetch_items_query).columns[0].values()[0] %}
+  {% set result = run_query(fetch_items_query) %}
+  {% set results_list = result.columns[0].values()[0] %}
   {{ run_query(results_list) }}
 {% endif %}
 
