8e1f641bb10477baec609344fea0e34b5b7a9e49
Sreeram Venkitesh <sreeram.venkitesh@bigbinary.com>
2022-03-04 15:21:08 +0530

    Config fix

--- macros/test.sql
+++ macros/test.sql
@@ -4,15 +4,14 @@
   select table_name
   from information_schema.tables
   where table_name like '_airbyte%'
-  and table_schema not in ('information_schema', 'pg_catalog')
   and table_type = 'BASE TABLE' 
 {% endset %}
 
 {% set sql %}
   drop table 
   {% for table in tables %}
-    {{ table }},
-    -- {% if not loop.last %},{% endif %}
+    {{ table }}
+    {% if not loop.last %},{% endif %}
   {% endfor %}
   cascade
 {% endset %}
